e7cf58284f6867cb85c0553ff480f8cd75776670
Lenny <lenny@itslennysfault.com>
2013-11-26 12:41:41 -0800

    restructured plugin to make everything cleaner.

--- src/jquery.quiccordion.js
+++ src/jquery.quiccordion.js
@@ -12,40 +12,44 @@
 
 (function($){
     $.fn.extend({
-         options:
-         {
-            activeClass: 'li.active',
-            preserveChildren: true,
-            openLevel: 0,
-            closedClass: 'closed',
-            childrenClass: 'has-children'
-         },
         quiccordion: function(options)
         {
-            var self=this;
-            $.extend(self.options, options);
+
+            var defaults={
+                activeClass: 'li.active',
+                preserveChildren: true,
+                openLevel: 0,
+                closedClass: 'closed',
+                childrenClass: 'has-children'
+            }        
+
+            $.extend(defaults, options);
             
             return this.each(function() {
-               var o=options;
-               self.initializeMenu(this);                   
+               new Quickcordion($(this),defaults);                   
             });
-        },
+        }
+    });
+        
+    //quickcordion class (keep the $ namespace clean)
+    var Quickcordion = function(obj,opts){
+        var options=opts;
+      
         //set up the menu... called on each object passed through
-        initializeMenu: function(obj)
+        var initializeMenu = function(obj)
         {
-            var self=this;         
-            var obj = $(this);
+
             var items = $("li", obj);
             $(obj).css('list-style','none').css('cursor','pointer');
             $("ul,li",obj).css('list-style','none').css('cursor','pointer');
-            self.initializeSubMenu(obj,0);
-        },
+            initializeSubMenu(obj,0);
+        }
+        
         //recursive function to set up recursive menu system
         //make all li's with sub ul's become clickable headers
-        initializeSubMenu: function(obj,depth)
+        var initializeSubMenu = function(obj,depth)
         {
             depth++;
-            var self=this;         
             obj.children("li:first").addClass("first");
             obj.children("li:last").addClass("last");            
             obj.children("li").each(function(){
@@ -53,47 +57,51 @@
                     e.stopPropagation();   
                     if($(this).children("ul").is(":visible"))
                     {
-                        self._closeItems($(this).children('ul:visible'));
+                        _closeItems($(this).children('ul:visible'));
                     }
                     else
                     {
                       $(this).parent().parent().siblings("li").each(function(){
-                         self._closeItems($(this).children('ul:visible'));
+                         _closeItems($(this).children('ul:visible'));
                       });
                       $(this).siblings("li").each(function(){
-                        self._closeItems($(this).children('ul:visible'));
+                        _closeItems($(this).children('ul:visible'));
                       });
-                      self._openItems($(this).children('ul'));
+                      _openItems($(this).children('ul'));
                     }
                 });
                //if it's in the active tree don't hide it
-               if($(this).find(self.options.activeClass).length == 0 && depth > self.options.openLevel)
+               if($(this).find(options.activeClass).length == 0 && depth > options.openLevel)
                {
                   $(this).children("ul").hide();
-                  $(this).addClass(self.options.closedClass);
+                  $(this).addClass(options.closedClass);
                }
 
                if($(this).children("ul").length >0)
                {
-                  $(this).addClass(self.options.childrenClass);
-                  self.initializeSubMenu($(this).children("ul"),depth);
+                  $(this).addClass(options.childrenClass);
+                  initializeSubMenu($(this).children("ul"),depth);
                }
             });
-        },
-        _closeItems: function(obj){
-            var self=this;
+        }
+        
+        var _closeItems = function(obj){
             obj.slideUp();
-            obj.parent().addClass(self.options.closedClass);
-             if(!self.options.preserveChildren)
+            obj.parent().addClass(options.closedClass);
+             if(!options.preserveChildren)
              {
                 obj.find("ul:visible").each(function(){
-                    self._closeItems($(this));
+                    _closeItems($(this));
                 });
              }
-        },
-        _openItems: function(obj){
-            obj.slideDown();
-            obj.parent().removeClass(this.options.closedClass);
         }
-    });
+        
+        var _openItems = function(obj){
+            obj.slideDown();
+            obj.parent().removeClass(options.closedClass);
+        }
+        
+        initializeMenu(obj);
+    }
+    
 })(jQuery);
